f3d68ec3ecb988303a52493c0d1335fe7c1910f0
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-11-12 22:57:25 -0300

    UPDATE: arrumando footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import './styles.css';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  root: {
-      fontSize:"2rem",
-      padding:"10px",
-      '&:hover':{
-        color:"#eb5e28",
-     
-      }
-  },
-  
-
-})
 
 
 function Footer() {
-  const classes = useStyles()
+
     return(
       <>
     <div className="social">
     <a rel="noopener noreferrer" target='_blank' href = "https://github.com/ErickMifo"  > 
-    <GitHubIcon className={classes.root} /> 
+    <GitHubIcon className='iconGit' /> 
     </a> 
         </div>
         </>
